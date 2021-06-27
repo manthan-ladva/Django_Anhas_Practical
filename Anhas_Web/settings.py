d20f4bbc7a0368d383c46daa19d3ac22f031be0e
Manthan Ladva <40834879+manthan-ladva@users.noreply.github.com>
2021-06-27 13:03:50 +0530

    Added GitIgnore

--- Anhas_Web/settings.py
+++ Anhas_Web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from Anhas_Web.secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'image_classification',
         'USER' : 'postgres',
-        'PASSWORD' : '****',
+        'PASSWORD' : password,
         'HOST' : 'localhost'
     }
 }
